410f5569c72b4de32b66137c5941a259fc4df8d1
PDv-github <paolo.devanna@gmail.com>
2020-04-08 13:14:55 +0200

    Updated file with working functions and comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
+# initiates a function called makeCacheMatrix that requires as argument a matrix x.
+# This function creates and stores an object that contains  x, invMatrix and the functions set(),
+# get(), setinverse(), getinverse()
+
 makeCacheMatrix <- function(x = matrix()) {
-
+  invMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) invMatrix <<- solve
+  getinverse <- function() invMatrix
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
+# this function rerieves x from the object created by makeCacheMatrix, then checks if a value
+# is already stored in memory. If this object exists, the function will return the value.
+# if not, the function will calculate the inverse of the x matrix.
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  invMatrix <- x$getinverse()
+  if (!is.null(invMatrix)) {
+    message("getting cached data")
+    return(invMatrix)
+  }
+  values <- x$get()
+  invMatrix <- solve(values, ...)
+  x$setinverse(invMatrix)
+  invMatrix
 }
+
